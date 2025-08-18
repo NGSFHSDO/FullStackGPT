78380a68e13d4863d06cd16f92750130d2db5430
HSDO <b82450717@gmail.com>
2025-08-18 22:08:17 +0900

    update: 2025-08-18 22:08

---
+++ Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from langchain.prompts import ChatPromptTemplate
+
+st.set_page_config(
+    page_title="God damm",
+    page_icon="👋",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+with st.sidebar:
+    st.title("God damm")
+
+
+st.write("Is it fucking possible?")
+    
+
+
+st.title("My First Streamlit App")
+
+
+
